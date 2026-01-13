c27f9152aa7852acae27338e74fa3db51ed96102
Starinin Andrey <starinin-andrey@ya.ru>
2026-01-13 20:33:19 +0300

    Добавление проекта Models

---
+++ Travels.Models/Point.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Travels.Models;
+
+public record Point
+{
+    public Guid Id { get; init; } = Guid.CreateVersion7();
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+    public required string Country { get; init; }
+}
